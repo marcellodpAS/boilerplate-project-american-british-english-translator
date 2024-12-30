8c9d6fc2ee3e52128d7c4e0bb37b43cdafa32741
marcellodpAS <marcello.depaola@apuliasoft.com>
2024-12-30 15:44:55 +0100

    exercise done

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,207 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+const translator = new Translator();
 
 suite('Unit Tests', () => {
+	suite('Translate to British English', () => {
+		test(`Translate "Mangoes are my favorite fruit." to British English`, () => {
+			const result = translator.translate(
+				'Mangoes are my favorite fruit.',
+				'american-to-british'
+			);
+			assert.equal(result.translation.includes('favourite'), true);
+		});
 
+		test(`Translate "I ate yogurt for breakfast." to British English`, () => {
+			const result = translator.translate(
+				'I ate yogurt for breakfast.',
+				'american-to-british'
+			);
+			assert.equal(result.translation.includes('yoghurt'), true);
+		});
+
+		test(`Translate "We had a party at my friend's condo." to British English`, () => {
+            const result = translator.translate(
+                `We had a party at my friend's condo.`,
+                `american-to-british`
+            );
+            assert.equal(result.translation.includes('flat'), true);
+        });
+
+        test(`Translate "Can you toss this in the trashcan for me?" to British English`, () => {
+            const result = translator.translate(
+                `Can you toss this in the trashcan for me?`,
+                `american-to-british`
+            );
+            assert.equal(result.translation.includes('bin'), true);
+        });
+
+        test(`Translate "The parking lot was full." to British English`, () => {
+            const result = translator.translate(
+                `The parking lot was full.`,
+                `american-to-british`
+            );
+            assert.equal(result.translation.includes('car park'), true);
+        });
+
+        test(`Translate "Like a high tech Rube Goldberg machine." to British English`, () => {
+            const result = translator.translate(
+                `Like a high tech Rube Goldberg machine.`,
+                `american-to-british`
+            );
+            assert.equal(result.translation.includes('Heath Robinson device'), true);
+        });
+
+        test(`Translate "To play hooky means to skip class or work." to British English`, () => {
+            const result = translator.translate(
+                `To play hooky means to skip class or work.`,
+                `american-to-british`
+            );
+            assert.equal(result.translation.includes('bunk off'), true);
+        });
+
+        test(`Translate "No Mr. Bond, I expect you to die." to British English`, () => {
+            const result = translator.translate(
+                `No Mr. Bond, I expect you to die.`,
+                `american-to-british`
+            );
+            assert.equal(result.translation.includes('Mr'), true);
+        });
+
+        test(`Translate "Dr. Grosh will see you now." to British English`, () => {
+            const result = translator.translate(
+                `Dr. Grosh will see you now.`,
+                `american-to-british`
+            );
+            assert.equal(result.translation.includes('Dr'), true);
+        });
+
+        test(`Translate "Lunch is at 12:15 today." to British English`, () => {
+            const result = translator.translate(
+                `Lunch is at 12:15 today.`,
+                `american-to-british`
+            );
+            assert.equal(result.translation.includes('12.15'), true);
+        });
+	});
+
+    suite('Translate to American English', () => {
+        test(`Translate "We watched the footie match for a while." to American English`, () => {
+            const result = translator.translate(
+                `We watched the footie match for a while.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('soccer'), true);
+        });
+
+        test(`Translate "Paracetamol takes up to an hour to work." to American English`, () => {
+            const result = translator.translate(
+                `Paracetamol takes up to an hour to work.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('Tylenol'), true);
+        });
+
+        test(`Translate "First, caramelise the onions." to American English`, () => {
+            const result = translator.translate(
+                `First, caramelise the onions.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('caramelize'), true);
+        });
+
+        test(`Translate "I spent the bank holiday at the funfair." to American English`, () => {
+            const result = translator.translate(
+                `I spent the bank holiday at the funfair.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('public holiday'), true);
+            assert.equal(result.translation.includes('carnival'), true);
+        });
+
+        test(`Translate "I had a bicky then went to the chippy." to American English`, () => {
+            const result = translator.translate(
+                `I had a bicky then went to the chippy.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('cookie'), true);
+            assert.equal(result.translation.includes('fish-and-chip shop'), true);
+        });
+
+        test(`Translate "I've just got bits and bobs in my bum bag." to American English`, () => {
+            const result = translator.translate(
+                `I've just got bits and bobs in my bum bag.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('odds and ends'), true);
+            assert.equal(result.translation.includes('fanny pack'), true);
+        });
+
+        test(`Translate "The car boot sale at Boxted Airfield was called off." to American English`, () => {
+            const result = translator.translate(
+                `The car boot sale at Boxted Airfield was called off.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('swap meet'), true);
+        });
+
+        test(`Translate "Have you met Mrs Kalyani?" to American English`, () => {
+            const result = translator.translate(
+                `Have you met Mrs Kalyani?`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('Mrs.'), true);
+        });
+
+        test(`Translate "Prof Joyner of King's College, London." to American English`, () => {
+            const result = translator.translate(
+                `Prof Joyner of King's College, London.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('Prof.'), true);
+        });
+
+        test(`Translate "Tea time is usually around 4 or 4.30." to American English`, () => {
+            const result = translator.translate(
+                `Tea time is usually around 4 or 4.30.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('4:30'), true);
+        });
+    })
+
+    suite('Highlight translations', () => {
+        test(`Highlight translations in "Mangoes are my favorite fruit."`, () => {
+			const result = translator.translate(
+				'Mangoes are my favorite fruit.',
+				'american-to-british'
+			);
+			assert.equal(result.translation.includes('class="highlight"'), true);
+		});
+
+		test(`Highlight translations in "I ate yogurt for breakfast."`, () => {
+			const result = translator.translate(
+				'I ate yogurt for breakfast.',
+				'american-to-british'
+			);
+			assert.equal(result.translation.includes('class="highlight"'), true);
+		});
+
+        test(`Highlight translations in "We watched the footie match for a while."`, () => {
+            const result = translator.translate(
+                `We watched the footie match for a while.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('class="highlight"'), true);
+        });
+
+        test(`Highlight translations in "Paracetamol takes up to an hour to work."`, () => {
+            const result = translator.translate(
+                `Paracetamol takes up to an hour to work.`,
+                `british-to-american`
+            );
+            assert.equal(result.translation.includes('class="highlight"'), true);
+        });
+    })
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,5 +8,101 @@ chai.use(chaiHttp);
 let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
+    suite('Translation POST request to /api/translate', () => {
+        test('With text and locale fields', (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'Mangoes are my favorite fruit',
+                    locale: 'american-to-british'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'text');
+                    assert.property(res.body, 'translation');
+                    assert.include(res.body.translation, `<span class="highlight">favourite</span>`);
+                    done();
+                })
+        });
 
+        test('With text and invalid locale field', (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'Mangoes are my favorite fruit.',
+                    locale: 'invalid-locale'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'error');
+                    assert.equal(res.body.error, 'Invalid value for locale field');
+                    done();
+                })
+        });
+
+        test('With missing text field', (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    locale: 'american-to-british'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'error');
+                    assert.equal(res.body.error, 'Required field(s) missing');
+                    done();
+                })
+        });
+
+        test('With missing locale field', (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'Mangoes are my favorite fruit'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'error');
+                    assert.equal(res.body.error, 'Required field(s) missing');
+                    done();
+                })
+        });
+
+        test('With empty text', (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: '',
+                    locale: 'american-to-british'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'error');
+                    assert.equal(res.body.error, 'No text to translate');
+                    done();
+                })
+        });
+
+        test('With text that needs no translation', (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'This text needs no translation',
+                    locale: 'american-to-british'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'text');
+                    assert.property(res.body, 'translation');
+                    assert.equal(res.body.translation, 'Everything looks good to me!');
+                    done();
+                })
+        });
+    });
 });
